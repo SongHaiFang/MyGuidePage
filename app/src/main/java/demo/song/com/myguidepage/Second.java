a18fc39c7f9311599c8f1ef9741a5649f94d21d6
Cor6 <1912167917@qq.com>
2017-09-15 18:24:41 +0800

    Initial commit

---
+++ app/src/main/java/demo/song/com/myguidepage/Second.java
@@ -0,0 +1,16 @@
+package demo.song.com.myguidepage;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * data:2017/9/15 0015.
+ * Created by ：宋海防  song on
+ */
+public class Second extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myact);
+    }
+}
